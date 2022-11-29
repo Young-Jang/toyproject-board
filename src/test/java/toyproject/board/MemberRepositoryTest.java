c912d88e8f32640e08315afa2ad643f623b11bad
201843 <b116104@shinsegae.com>
2022-11-29 22:05:53 +0900

    feat: set db connection and controller

---
+++ src/test/java/toyproject/board/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package toyproject.board;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId().equals(member.getId()));
+        Assertions.assertThat(findMember.getUsername().equals(member.getUsername()));
+    }
+
+}
