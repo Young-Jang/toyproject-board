3d2cb5c424402398e0ba30668fd8ebe0d4606b8a
201843 <b116104@shinsegae.com>
2022-12-01 23:11:37 +0900

    feat: add swagger ui

--- src/main/java/toyproject/board/rest/HelloController.java
+++ src/main/java/toyproject/board/rest/HelloController.java
@@ -1,17 +1,18 @@
 package toyproject.board.rest;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import toyproject.board.application.PatchMemberService;
 
 import javax.json.JsonMergePatch;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class HelloController {
 
@@ -23,9 +24,14 @@ public class HelloController {
         return "hello";
     }
 
-
+    //TODO PATCH ERROR 조치
+    @ApiOperation(value="member entity patch", notes="member entity 개별 컬럼들을 patch한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "API 정상 작동"),
+            @ApiResponse(code = 500, message = "서버 에러")
+    })
     @PatchMapping("/member/{id}")
-    public String patchMember(@PathVariable Long id, @RequestBody JsonMergePatch jsonMergePatch){
+    public String patchMember(@PathVariable Long id, @JsonProperty("email") @RequestBody JsonMergePatch jsonMergePatch){
         patchMemberService.patchMemberInfo(id,jsonMergePatch);
         return "hello";
     }
