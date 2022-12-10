ffd628827d449eea840f877f5476893dd7c1ad2e
b116104@naver.com <b116104@naver.com>
2022-12-10 23:53:54 +0900

    feat: add lotto success test

--- src/test/java/toyproject/board/application/LottoServiceTest.java
+++ src/test/java/toyproject/board/application/LottoServiceTest.java
@@ -30,18 +30,21 @@ class LottoServiceTest {
         JSONObject jsonObject = (JSONObject) obj;
 
         jsonObject.get("drwtNo");
-        int count = 0;
-        for(int i = 1; i < 7; i++){
-            List<Integer> lottoList = lottoService.createLottoNumber();
-            System.out.println(lottoList.get(i));
-            System.out.println(jsonObject.get("drwtNo"+i));
-            if(!lottoList.get(i).equals(jsonObject.get("drwtNo"+i))){
-                i=1;
-                continue;
-            }
-            count++;
-        }
-        System.out.println(count);
+        int count = 1;
+        boolean endFlag = false;
+        while(!endFlag){
+            List<String> lottoList = lottoService.createLottoNumber(1);
+            String result = lottoList.get(0).substring(1,lottoList.get(0).length()-1);
+            String[] strings = result.split("-");
+            for(int i = 1; i < 7; i++){
+                if(!strings[i-1].equals(jsonObject.get("drwtNo"+i))){
+                    break;
+                }
+                if(i==6)
+                    endFlag = true;
 
+            }
+            System.out.println(count++);
+        }
     }
 }
