a0e24bbd0695c180f756ef837b4b380d9737ab61
b116104@naver.com <b116104@naver.com>
2022-12-23 00:46:36 +0900

    feat: add API URL constants

--- src/main/java/toyproject/almigty/board/rest/BoardController.java
+++ src/main/java/toyproject/almigty/board/rest/BoardController.java
@@ -10,6 +10,8 @@ import toyproject.almigty.board.rest.DTO.BoardDto;
 
 import java.util.List;
 
+import static toyproject.almigty.common.constants.BoardApiUrl.*;
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("board")
@@ -22,7 +24,7 @@ public class BoardController {
     // list 경로로 GET 메서드 요청이 들어올 경우 list 메서드와 맵핑시킴
     // list 경로에 요청 파라미터가 있을 경우 (?page=1), 그에 따른 페이지네이션을 수행함.
 
-    @GetMapping({"", "/list"})
+    @GetMapping({"", GET_BOARD_LIST})
     public String list(Model model, @RequestParam(value="page", defaultValue = "1") Integer pageNum) {
         log.info("[GET] /list api 호출");
         List<BoardDto> boardList = boardService.getBoardlist(pageNum);
@@ -36,7 +38,7 @@ public class BoardController {
 
     // 글쓰는 페이지
 
-    @GetMapping("/post")
+    @GetMapping(GET_BOARD_POST_PAGE)
     public String write() {
         log.info("[GET] /post api 호출");
         return "board/write";
@@ -45,7 +47,7 @@ public class BoardController {
     // 글을 쓴 뒤 POST 메서드로 글 쓴 내용을 DB에 저장
     // 그 후에는 /list 경로로 리디렉션해준다.
 
-    @PostMapping("/post")
+    @PostMapping(POST_BOARD_POST_PAGE)
     public String write(BoardDto boardDto) {
         log.info("[POST] /post api 호출");
         boardService.savePost(boardDto);
@@ -55,7 +57,7 @@ public class BoardController {
     // 게시물 상세 페이지이며, {no}로 페이지 넘버를 받는다.
     // PathVariable 애노테이션을 통해 no를 받음
 
-    @GetMapping("/post/{no}")
+    @GetMapping(GET_BOARD_DETAIL)
     public String detail(@PathVariable("no") Long no, Model model) {
         log.info("[GET] /post/{no} api 호출");
 
@@ -67,7 +69,7 @@ public class BoardController {
 
     // 게시물 수정 페이지이며, {no}로 페이지 넘버를 받는다.
 
-    @GetMapping("/post/edit/{no}")
+    @GetMapping(GET_BOARD_EDIT_PAGE)
     public String edit(@PathVariable("no") Long no, Model model) {
         log.info("[GET] /post/edit/{no} api 호출");
 
@@ -79,7 +81,7 @@ public class BoardController {
 
     // 위는 GET 메서드이며, PUT 메서드를 이용해 게시물 수정한 부분에 대해 적용
 
-    @PostMapping("/post/edit")
+    @PostMapping(POST_EDIT)
     public String update(BoardDto boardDTO) {
         log.info("[Put] /post/edit/{no} api 호출");
 
@@ -90,7 +92,7 @@ public class BoardController {
 
     // 게시물 삭제는 deletePost 메서드를 사용하여 간단하게 삭제할 수 있다.
 
-    @PostMapping("/post/delete/{no}")
+    @PostMapping(POST_DELETE)
     public String delete(@PathVariable("no") Long no) {
         log.info("[Delete] /post/{no} api 호출");
 
@@ -103,7 +105,7 @@ public class BoardController {
     // keyword를 view로부터 전달 받고
     // Service로부터 받은 boardDtoList를 model의 attribute로 전달해준다.
 
-    @GetMapping("/search")
+    @GetMapping(GET_SEARCH)
     public String search(@RequestParam(value="keyword") String keyword, Model model) {
         log.info("[Get] /board/search api 호출");
 
---
+++ src/main/java/toyproject/almigty/common/constants/BoardApiUrl.java
@@ -0,0 +1,12 @@
+package toyproject.almigty.common.constants;
+
+public class BoardApiUrl {
+    public static final String GET_BOARD_LIST = "/list";
+    public static final String GET_BOARD_POST_PAGE = "/post";
+    public static final String POST_BOARD_POST_PAGE = "/post";
+    public static final String GET_BOARD_DETAIL = "/post/{no}";
+    public static final String GET_BOARD_EDIT_PAGE = "/post/edit/{no}";
+    public static final String POST_EDIT = "/post/edit";
+    public static final String POST_DELETE = "/post/delete/{no}";
+    public static final String GET_SEARCH = "/search";
+}
