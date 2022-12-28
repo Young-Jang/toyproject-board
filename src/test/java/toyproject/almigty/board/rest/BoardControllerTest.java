b4589154621ea89564152c80ee2671410951ade5
b116104@naver.com <b116104@naver.com>
2022-12-28 23:06:06 +0900

    feat: add controller test

--- src/test/java/toyproject/almigty/board/rest/BoardControllerTest.java
+++ src/test/java/toyproject/almigty/board/rest/BoardControllerTest.java
@@ -1,16 +1,56 @@
 package toyproject.almigty.board.rest;
 
-import lombok.extern.slf4j.Slf4j;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import toyproject.almigty.board.application.BoardService;
+import toyproject.almigty.board.application.CustomOAuth2UserService;
+import toyproject.almigty.board.application.GetUserInfoService;
+import toyproject.almigty.board.application.RestJsonService;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static toyproject.almigty.common.constants.BoardApiUrl.GET_BOARD_LIST;
 
-@Slf4j
+@MockBean(JpaMetamodelMappingContext.class)
+@WebMvcTest(BoardController.class)
+@AutoConfigureMockMvc
 class BoardControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @MockBean
+    private BoardService boardService;
+    @MockBean
+    private RestJsonService restJsonService;
+    @MockBean
+    private GetUserInfoService getUserInfoService;
+    @MockBean
+    private CustomOAuth2UserService customOAuth2UserService;
+    @MockBean
+    private ClientRegistrationRepository clientRegistrationRepository;
 
     @Test
     void JsonParserTest() throws ParseException {
@@ -18,6 +58,32 @@ class BoardControllerTest {
         JSONParser parser = new JSONParser();
         JSONObject jsonObject =(JSONObject) parser.parse(jsonString);
         System.out.println((String) jsonObject.get("access_token"));
+    }
+
+    @Test
+    @DisplayName("게시판 리스트를 조한다.")
+    void getList() throws Exception{
+        //given
+
+        //when
+        ResultActions result = this.mockMvc.perform(
+                MockMvcRequestBuilders.get(GET_BOARD_LIST)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+
+        //then
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
