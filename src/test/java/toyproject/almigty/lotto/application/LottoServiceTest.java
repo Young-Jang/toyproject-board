b96495b5623d575f3a82262239f6467ad047bf47
b116104@naver.com <b116104@naver.com>
2023-02-09 22:06:26 +0900

    test: multiThread threadSafe test

--- src/test/java/toyproject/almigty/lotto/application/LottoServiceTest.java
+++ src/test/java/toyproject/almigty/lotto/application/LottoServiceTest.java
@@ -1,6 +1,7 @@
 package toyproject.almigty.lotto.application;
 
 import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.DisplayName;
@@ -13,9 +14,14 @@ import toyproject.almigty.lotto.application.LottoService;
 import toyproject.almigty.lotto.infrastructure.LottoFeignClient;
 
 import java.io.UnsupportedEncodingException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
 class LottoServiceTest {
@@ -82,5 +88,34 @@ class LottoServiceTest {
             }
         }
         return cardNickname;
+    }
+
+    @Test
+    public void ArrayThreadSafeTest(){
+        //given
+        List<String> list = new ArrayList<>();
+        List<String> normalList = new ArrayList<>();
+        List<String> synchronizedList = Collections.synchronizedList(new ArrayList<>());
+
+        for(int i = 0;i<10000;i++)
+            list.add(String.valueOf(i));
+
+        List<CompletableFuture<Void>> testFuture = list.stream()
+                .map(num -> CompletableFuture.runAsync(() -> {
+                    normalList.add(num);
+                    synchronizedList.add(num);
+                }))
+                .collect(Collectors.toList());
+
+        /* when */
+        List<Void> testSync = CompletableFuture.allOf(testFuture.toArray(new CompletableFuture[0]))
+                .thenApply(Void -> testFuture.stream()
+                        .map(CompletableFuture::join)
+                        .collect(Collectors.toList()))
+                .join();
+
+        //then
+        assertEquals(list.size(),synchronizedList.size());
+        assertNotEquals(list.size(),normalList.size());
     }
 }
